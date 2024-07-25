b2921c560ebbe82c16fc765a7d8661f1bfb6bbf4
cerico <garethrobertlee@gmail.com>
2024-07-25 08:12:42 +0100

    feat: update urls list

--- urls.js
+++ urls.js
@@ -1,6 +1,16 @@
 module.exports = {
   urls: [
+    { url: 'www.picfair.com' },
+    { url: 'www.gov.uk' },
+    { url: 'cityguessr.com' },
+    { url: 'geocircles.cc' },
+    { url: 'pop-gdp.netlify.app' },
+    { url: 'slide-layout.netlify.app/' },
+    { url: 'dev.io37.ch' },
+    { url: 'observatories.io37.ch' },
     { url: 'www.boots.com' },
-    { url: 'www.itv.com' }
+    { url: 'www.itv.com' },
+    { url: 'autobuskastanicasarajevo.ba/red-voznje-sarajevo' },
+    { url: 'typology.netlify.app/' }
   ]
-};
+}
